545fd0bc8b85d9e16f106f193f09bfe6eefaad37
Creeper011 <filho11@icloud.com>
2025-12-24 18:29:55 -0300

    typo: fix to use config.yaml instead config.yml

--- src/core/constants.py
+++ src/core/constants.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 
-DEFAULT_YAML_CONFIG_PATH = Path("config.yml")
+DEFAULT_YAML_CONFIG_PATH = Path("config.yaml")
 YAML_FILE_ENCODING = "UTF-8"
 DEFAULT_ENV_CONFIG_PATH = Path(".env")
 DEFAULT_LOADERS_PATH = Path("src/infrastructure/services/config/loaders")
